d391929392be171a16371d9c91f68400f2ab7ec4
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2025-12-30 21:08:16 +0545

    Part 2 Q1 Completed

---
+++ Week 6/Part 2/1.py
@@ -0,0 +1,10 @@
+def types(val):
+    integer_val = int(val)
+    print("Value in integer: ", integer_val)
+
+    float_val = float(val)
+    print("Value in Float: ", float_val)
+
+types("123")
+
+
